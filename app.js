69cb4d9323105f609d385eb5cd509b3aca12f8d4
Ankush Dua <ankushduacodes@gmail.com>
2022-01-28 00:26:04 +0530

    added cors policy to accept requests from selected routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import dotenv from 'dotenv';
+import cors from 'cors';
 import dbConnect from './src/db/index.js';
 import UserRoutes from './src/router/userRoutes.js';
 import todoRoutes from './src/router/todoRoutes.js';
@@ -10,12 +11,31 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8001;
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true,
 }));
 dbConnect();
+const allowedOrigins = [
+  'http://localhost:8080',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Origin not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 204,
+};
+
+// @ts-ignore
+app.options('*', cors(corsOptions));
+// @ts-ignore
+app.use(cors(corsOptions));
 app.use('/api/v1/user/', UserRoutes);
 app.use('/api/v1/todo/', todoRoutes);
 
