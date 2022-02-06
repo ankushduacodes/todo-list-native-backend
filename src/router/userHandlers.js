ccee8f863b363ce2fdbac1cdb218c892e200411f
Ankush Dua <ankushduacodes@gmail.com>
2022-02-07 00:14:06 +0530

    Update userHandlers.js

--- src/router/userHandlers.js
+++ src/router/userHandlers.js
@@ -54,14 +54,12 @@ export async function registerHandler(req, res) {
       return res.status(409).json({ message: 'User already exists, Please login' });
     }
     const encryptedPassword = await generateHash(password);
-    console.log(encryptedPassword);
     const newUser = await new User({
       firstName,
       lastName,
       email,
       password: encryptedPassword,
     });
-    console.log(newUser);
     await newUser.save();
     return res.json({ message: 'user created' });
   } catch (err) {
